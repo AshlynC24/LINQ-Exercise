5f8de71b338952c51422a308b61cc514a0e082d1
Ashlyn Carmosino <ashlyncarmosino@Ashlyns-Air.lan>
2023-02-22 14:13:13 -0500

    Exercise 1

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace LINQ
+﻿using System.Linq;
+
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //list of video game names 
+            //order list of games by length of game name
+            //use => expression and method syntax
+            var GameNames = new List<string>() { "Super Mario", "COD", "Rocket League", "Destiny", "Diablo" };
+
+           
+            GameNames.OrderBy(x => x.Length).ToList().ForEach(x => Console.WriteLine(x));
         }
+        
     }
 }
